4f85bf53a4a92ea43e7eb1dce057c497f2637611
kosuke <kosuke@wake.org>
2016-10-10 15:03:17 +0900

    Initial Commit

---
+++ myFirstGithub93/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  myFirstGithub93
+//
+//  Created by kosuke on 2016/10/10.
+//  Copyright © 2016年 Studio Clock Moon Night. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
